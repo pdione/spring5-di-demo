7f331f4d8369a09da1a6226f02dfb3df02acd8c8
pdione <dione.pape@gmail.com>
2017-10-11 11:35:41 +0200

    Initial commit of Di Demo

---
+++ src/main/java/group/springframework/DiDemoApplication.java
@@ -0,0 +1,19 @@
+package group.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import group.springframework.controllers.MyController;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+	ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+	MyController controller = (MyController) ctx.getBean("myController");
+
+	controller.hello();
+    }
+}
---
+++ src/main/java/group/springframework/controllers/MyController.java
@@ -0,0 +1,13 @@
+package group.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello() {
+	System.out.println("Hello!!! ");
+
+	return "foo";
+    }
+}
